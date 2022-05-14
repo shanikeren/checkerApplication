332d66dd0d10f18c3ed0d5add20a78259ab1ca92
shanikeren <81972962+shanikeren@users.noreply.github.com>
2022-05-14 15:57:39 +0300

    2

--- checkerApplication/checkerApplication/App.xaml.cs
+++ checkerApplication/checkerApplication/App.xaml.cs
@@ -6,6 +6,8 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using System.Threading.Tasks;
 using CheckerDTOs;
+using System.Text.Json;
+using System.Collections.Generic;
 
 namespace checkerApplication
 {
@@ -13,9 +15,13 @@ namespace checkerApplication
     {
         public static HubConnection HubConn { get; private set; }
         public MainPage mainPage = new MainPage();
+        CheckerHttpClient checkerHttpClient = new CheckerHttpClient("http://localhost:7059");
+        List<Restaurant> restaurants = new List<Restaurant>();
+
         public App()
         {
             InitializeComponent();
+
 
             HubConn = new HubConnectionBuilder()
                 .WithUrl("http://localhost:7059/restaurants")
@@ -23,17 +29,18 @@ namespace checkerApplication
 
             HubConn.Closed += async (error) =>
             {
-                await Task.Delay(new Random().Next(0,5) * 1000);
+                await Task.Delay(new Random().Next(0, 5) * 1000);
                 await HubConn.StartAsync();
-               
+
             };
 
-
-            MainPage = new NavigationPage(new MainPage()) { };
+            _ = GetItemsAsync();
+            Console.WriteLine(restaurants);
+           /* MainPage = new NavigationPage(new MainPage()) { };
             DependencyService.Register<MockDataStore>();
             MainPage = new NavigationPage(mainPage) {
                 BarBackgroundColor = Color.DarkBlue,BarTextColor = Color.White
-            };
+            };*/
         }
 
 
@@ -48,6 +55,25 @@ namespace checkerApplication
         protected override void OnResume()
         {
         }
-      
+
+        public async Task<IEnumerable<Restaurant>> GetItemsAsync(bool forceRefresh = false)
+        {
+            // generate the response into a ToDoList object (list of the DTO with prettier printing)
+            /*HttpResponseMessage res = (await client.SendRequest());
+            if (res.IsSuccessStatusCode)
+            {
+                HttpContent content = res.Content;
+
+                List<ToDo> listy = JsonSerializer.Deserialize<List<ToDo>>(await content.ReadAsStringAsync());
+                return listy;
+            }
+            else
+            {
+                return new List<ToDo>();
+            }*/
+            restaurants = JsonSerializer.Deserialize<List<Restaurant>>(await checkerHttpClient.SendRequest());
+            return restaurants;
+        }
+
     }
 }
---
+++ checkerApplication/checkerApplication/CheckerHttpClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checkerApplication
+{
+    internal class CheckerHttpClient
+    {
+        
+            private readonly HttpClient checker;
+            public CheckerHttpClient(String url)
+            {
+                checker = new HttpClient();
+                checker.BaseAddress = new Uri(url);
+                checker.Timeout = TimeSpan.FromMinutes(5);
+            }
+
+            // returns the content of the response as a string
+            public async Task<String> SendRequest()
+            {
+                // get the content of the response
+                return await checker.GetStringAsync("/restaurants");
+            }
+        }
+   
+}

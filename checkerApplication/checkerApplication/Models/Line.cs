e4078911a8feb393dc76ddc6b87ae73fe1e0f4af
shanikeren <81972962+shanikeren@users.noreply.github.com>
2022-05-23 20:41:43 +0300

    line added

---
+++ checkerApplication/checkerApplication/Models/Line.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace checkerApplication.Models
+{
+    internal class Line
+    {
+        
+            private List<OrderItem> Locked = new List<OrderItem>();
+            private List<OrderItem> ToDo = new List<OrderItem>();
+            private List<OrderItem> Doing = new List<OrderItem>();
+
+          
+            public string Name { get; set; }
+
+          
+            public int ServingAreaId { get; set; }
+
+    
+            public int Limit { get; set; } = -1;
+
+
+            public LineState State { get; set; } = LineState.Closed;
+
+            public List<Dish> Dishes { get; set; } = new List<Dish>();
+    
+
+    }
+}
